a2c711f98d338dedd1132d7d2cfa138b7e283cbe
de110 <skyyks98@gmail.com>
2022-04-27 18:09:52 +0900

    update

--- src/main/java/com/example/chatserver/controller/MessageController.java
+++ src/main/java/com/example/chatserver/controller/MessageController.java
@@ -1,7 +1,15 @@
 package com.example.chatserver.controller;
 
+import com.example.chatserver.model.ChatMessage;
+import com.example.chatserver.service.ChatService;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -12,11 +20,21 @@ public class MessageController {
 
     private final SimpMessageSendingOperations sendingOperations;
 
+    private final ChatService chatService;
+
+    @PostMapping("/chat/message")
     @MessageMapping("/chat/message")
-    public void enter(ChatMessage message) {
+    public ChatMessage enter(@RequestBody ChatMessage message) {
         if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
             message.setMessage(message.getSender() + "님이 입장하였습니다.");
         }
-        sendingOperations.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        sendingOperations.convertAndSend("/sub/chat/room/" + message.getRoomId(),
+                message);
+
+        return chatService.saveMessage(message);
+        // ChatMessage chatMessage = new ChatMessage();
+        // return message;
+
     }
+
 }
