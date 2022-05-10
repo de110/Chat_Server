7b7bf375db03efd634bb496a0af68e878d011ee6
de110 <skyyks98@gmail.com>
2022-05-10 18:41:42 +0900

    update

--- vue/vue-front/src/components/room.vue
+++ vue/vue-front/src/components/room.vue
@@ -1,5 +1,5 @@
 <template>
-  <!DOCTYPE html>
+  <!-- <!DOCTYPE html>
   <html
     lang="en"
     xmlns:v-on="http://www.w3.org/1999/xhtml"
@@ -11,69 +11,70 @@
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-      />
-      <!-- <link rel="stylesheet" href="/webjars/bootstrap/4.3.1/dist/css/bootstrap.min.css"> -->
-      <style>
+      /> -->
+  <!-- <link rel="stylesheet" href="/webjars/bootstrap/4.3.1/dist/css/bootstrap.min.css"> -->
+  <!-- <style>
         [v-cloak] {
           display: none;
         }
       </style>
     </head>
-    <body>
-      <div class="container" id="app" v-cloak>
-        <div class="row">
-          <div class="col-md-12">
-            <h3>채팅방 리스트</h3>
-          </div>
-        </div>
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <label class="input-group-text">방제목</label>
-          </div>
-          <input
-            type="text"
-            class="form-control"
-            v-model="room_name"
-            v-on:keyup.enter="createRoom"
-          />
-          <div class="input-group-append">
-            <button class="btn btn-primary" type="button" @click="createRoom">
-              채팅방 개설
-            </button>
-          </div>
-        </div>
-        <ul class="list-group">
-          <li
-            class="list-group-item list-group-item-action"
-            v-for="item in chatrooms"
-            v-bind:key="item.roomId"
-            v-on:click="enterRoom(item.roomId)"
-          >
-            {{ item.roomName }}
-          </li>
-        </ul>
+    <body> -->
+  <div class="container" id="app" v-cloak>
+    <div class="row">
+      <div class="col-md-12">
+        <h3>채팅방 리스트</h3>
       </div>
-      <script src="/webjars/vue/2.5.16/vue.min.js"></script>
+    </div>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <label class="input-group-text">방제목</label>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        v-model="room_name"
+        v-on:keyup.enter="createRoom"
+      />
+      <div class="input-group-append">
+        <button class="btn btn-primary" type="button" @click="createRoom">
+          채팅방 개설
+        </button>
+      </div>
+    </div>
+    <ul class="list-group">
+      <li
+        class="list-group-item list-group-item-action"
+        v-for="item in chatrooms"
+        :key="item.roomId"
+        @click="enterRoom(item.roomId)"
+      >
+        {{ item.roomName }}
+      </li>
+    </ul>
+  </div>
+  <!-- <script src="/webjars/vue/2.5.16/vue.min.js"></script>
       <script src="/webjars/axios/0.17.1/dist/axios.min.js"></script>
     </body>
-  </html>
+  </html> -->
 </template>
 <script>
 import axios from "axios";
 export default {
   el: "#app",
-  data: {
-    room_name: "",
-    chatrooms: []
+  data() {
+    return {
+      room_name: "",
+      chatrooms: []
+    };
   },
   created() {
     this.findAllRoom();
   },
   methods: {
-    findAllRoom: function() {
-      axios.get("/chat/rooms").then(response => {
+    async findAllRoom() {
+      await axios.get("/chat/rooms").then(response => {
         this.chatrooms = response.data;
-        console.log(this.chatrooms);
       });
     },
     createRoom: function() {
@@ -97,11 +98,12 @@ export default {
           });
       }
     },
-    enterRoom: function(roomId) {
+    enterRoom(roomId) {
       var sender = prompt("대화명을 입력해 주세요.");
       if (sender !== "") {
         localStorage.setItem("wschat.sender", sender);
         localStorage.setItem("wschat.roomId", roomId);
+        // this.$router.push(`/chat/room/enter/${roomId}`)
         location.href = "/chat/room/enter/" + roomId;
         // location.href="/chat/room/enter/";
       }
